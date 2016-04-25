61763ec6155ad4c8bfa4eab556505cd011ce0afb
Luke Sorenson <lasoren@me.com>
2016-04-25 02:46:13 -0400

    Adding new short data file cause the other one seems messed up

--- perceptron/perceptron.c
+++ perceptron/perceptron.c
@@ -73,7 +73,7 @@ int main(int argc, const char** argv){
     
     int line_counter = 0;
 
-    FILE* stream = fopen("data_short.csv", "r");
+    FILE* stream = fopen("data_short2.csv", "r");
 
     char line[1024];
     while (fgets(line, 1024, stream))
