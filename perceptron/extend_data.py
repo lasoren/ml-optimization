b910bbf08045d173e053d3aa44625ebcc9d887b0
Luke Sorenson <lasoren@me.com>
2016-04-25 03:23:09 -0400

    Extending the original data set

--- perceptron/extend_data.py
+++ perceptron/extend_data.py
@@ -1,6 +1,6 @@
 import random
 
 with open('data.csv', 'a') as f:
-    for i in xrange(4500):
+    for i in xrange(500):
         f.write(str(random.random()) + ", " + str(random.random()) + ",\n")
 
