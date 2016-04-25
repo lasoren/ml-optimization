6601fafe6c788721c8979718199d25bcaf90d49d
Luke Sorenson <lasoren@me.com>
2016-04-25 02:57:07 -0400

    dont need diff function in perceptron_pt

--- perceptron/perceptron_pt.c
+++ perceptron/perceptron_pt.c
@@ -35,18 +35,6 @@ struct thread_data{
   int NUM_THREADS;
 };
 
-struct timespec diff(struct timespec start, struct timespec end){
-  struct timespec temp;
-  if ((end.tv_nsec-start.tv_nsec)<0) {
-    temp.tv_sec = end.tv_sec-start.tv_sec-1;
-    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-  } else {
-    temp.tv_sec = end.tv_sec-start.tv_sec;
-    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-  }
-  return temp;
-}
-
 // helper function for multithreaded perceptron
 void* perceptron_helper(void* threadarg){
     // pass the thread a struct containing its thread_id, X and y data, length,
